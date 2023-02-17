eaf524703a4c7b978f425ff26702a7061b6eb0ed
GlobalOffender <99067159@mydavinci.nl>
2023-02-17 11:05:28 +0100

    Website

---
+++ AllScripts.js
@@ -0,0 +1,28 @@
+function HeaderText() {
+    document.getElementById("Header").innerHTML = "GEHAKTBROOD"
+}
+function Paragraph1Text() {
+    document.getElementById("Paragraph1Text").innerHTML = "Dit gehaktbrood kan prima bij een avondmaaltijd, maar is ook een makkelijk gerecht voor een buffet."
+}
+function Title2Text() {
+    document.getElementById("Title2").innerHTML = "Ingredienten"
+}
+        "1 rode paprika",
+        "1 groene paprika",
+        "1 ui",
+        "2 tenen knoflook",
+        "50 gram bacon",
+        "1/2 bosje peterselie",
+        "400 gram mager rundergehakt",
+        "50 gram paneermeel",
+        "1 ei",
+        "1 blikje tomatenpuree",
+        "1 theelepel tabasco",
+        "peper"
+function Title3Text() {
+    document.getElementById("Title3").innerHTML = "Bereidingswijze"
+}
+HeaderText();
+Paragraph1Text();
+Title2Text();
+Title3Text();
